070ed838e8c2cec9c122d07ae7f3f7e9b44128f3
KongYu <1031545083@qq.com>
2017-01-13 00:24:36 +0800

    Homepage has  finished.it can query all of the matches.

---
+++ iPE/Models/HomeViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iPE.Models
+{
+    public class HomeSearchModel
+    {
+        public string name
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Data5
+    {
+        public string time { get; set; }
+        public string team1 { get; set; }
+        public string team2 { get; set; }
+        public string state { get; set; }
+        public string score { get; set; }
+        public string src { get; set; }
+    }
+}
